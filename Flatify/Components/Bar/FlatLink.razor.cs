c0e09f777c9e19d76909737daa2940dcce7aeece
Oliver Conover <fuchsfarbeart@outlook.com>
2023-12-26 02:39:59 -0500

    Enhance FlatLink component and add FlatSearch component
    The FlatLink component has been enhanced with NavigationManager usage, status tracking, and improved CSS handling. A new search component, FlatSearch, has also been introduced. Changes in the NavMenu include switching the FlatLink components to menu items.

--- Flatify/Components/Bar/FlatLink.razor.cs
+++ Flatify/Components/Bar/FlatLink.razor.cs
@@ -1,20 +1,25 @@
 using Flatify.Utilities;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.Extensions.Logging;
 
 namespace Flatify
 {
     /// <summary>
     ///     Creates a navigation link that is styled as a flat button.
     /// </summary>
-    public partial class FlatLink
+    public partial class FlatLink : IDisposable
     {
         public string Classname => new CssBuilder()
-                                   .AddClass("menu-item")
+                                   .AddClass("menu-item", MenuItem)
                                    .AddClass("flex-center-x", !MenuItem)
+                                   .AddClass("active", IsActive)
                                    .AddClass(Class)
                                    .Build();
 
+        [Inject] public NavigationManager Nav { get; set; }
+
+        private bool IsActive => IsActiveUri();
         /// <summary>
         ///     The child content of the navigation bar.
         /// </summary>
@@ -38,5 +43,38 @@ namespace Flatify
         /// How routing is matched for setting the active class.
         /// </summary>
         [Parameter] public NavLinkMatch MatchMode { get; set; } = NavLinkMatch.All;
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            Nav.LocationChanged -= ChangeState;
+        }
+
+        /// <inheritdoc />
+        protected override void OnInitialized()
+        {
+            Nav.LocationChanged += ChangeState;
+        }
+
+        private void ChangeState(object sender, LocationChangedEventArgs e)
+        {
+            StateHasChanged();
+        }
+
+        /// <inheritdoc />
+        protected override async Task OnInitializedAsync()
+        {
+            UserAttributes.Add("class", "menu-item");
+            UserAttributes.Add("href", Link);
+            await base.OnInitializedAsync();
+        }
+
+        // Test relative uri for active state
+        public bool IsActiveUri()
+        {
+            var relUri = Nav.Uri.Replace(Nav.BaseUri, "/");
+            Logger.LogInformation("Relative URI: {0}", relUri);
+            return relUri.StartsWith(Link);
+        }
     }
 }
